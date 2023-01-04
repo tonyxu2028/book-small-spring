0f4d00bc5247f646c238c4a5334d2058df1360a0
tonyxu2028 <liwei2028@163.com>
2023-01-04 11:05:17 +0800

    spring-step-13

--- spring-step-13/src/test/java/cn/bugstack/springframework/test/SpringStep13Test.java
+++ spring-step-13/src/test/java/cn/bugstack/springframework/test/SpringStep13Test.java
@@ -20,14 +20,16 @@ public class SpringStep13Test {
 
     @Test
     public void test_scan() {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-scan.xml");
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("classpath:spring-scan.xml");
         IUserService userService = applicationContext.getBean("userService", IUserService.class);
         System.out.println("测试结果：" + userService.queryUserInfo());
     }
 
     @Test
     public void test_property() {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-property.xml");
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("classpath:spring-property.xml");
         IUserService userService = applicationContext.getBean("userService", IUserService.class);
         System.out.println("测试结果：" + userService);
     }
